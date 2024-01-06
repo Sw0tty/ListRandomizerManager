71b3834a1e84388747e4090f74d75f12d781e170
Swotty <swotty@yandex.ru>
2024-01-06 11:19:22 +0300

    excel Export\Import

--- RandomGame/Form1.Designer.cs
+++ RandomGame/Form1.Designer.cs
@@ -36,6 +36,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
@@ -56,7 +58,7 @@
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(18, 146);
+            this.button1.Location = new System.Drawing.Point(18, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(234, 84);
             this.button1.TabIndex = 1;
@@ -68,7 +70,7 @@
             // 
             this.label1.Cursor = System.Windows.Forms.Cursors.Default;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(18, 88);
+            this.label1.Location = new System.Drawing.Point(18, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(234, 25);
             this.label1.TabIndex = 2;
@@ -124,11 +126,33 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Название игры";
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 28);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Export to Excel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(143, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 28);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Import from Excel";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(489, 426);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
@@ -156,6 +180,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- RandomGame/Form1.cs
+++ RandomGame/Form1.cs
@@ -5,10 +5,16 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ClosedXML.Excel;
+using System.Runtime.InteropServices.ComTypes;
+using DocumentFormat.OpenXml.Spreadsheet;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
 
 namespace RandomGame
 {
@@ -89,6 +95,66 @@ namespace RandomGame
             coll.AddRange(listBox1.Items.Cast<string>().ToArray());
             Properties.Settings.Default.listBoxValues = coll;
             Properties.Settings.Default.Save();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var wb = new XLWorkbook();
+            var ws = wb.Worksheets.Add("My games");
+            
+/*            // Title
+            ws.Cell("A1").Value = "All my games";*/
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                ws.Cell($"A{i + 1}").Value = listBox1.Items[i].ToString();
+            }
+            ws.Columns().AdjustToContents();
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+            {
+                wb.SaveAs($"{dialog.SelectedPath}\\MyGames.xlsx");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                var workbook = new XLWorkbook(filePath);
+                var ws = workbook.Worksheet(1);
+                int count = 0;
+
+                for (int i = 1; i < ws.RowCount(); i++)
+                {
+                    var row = ws.Row(i);
+                    string cellValue = row.Cell(1).Value.ToString();
+
+                    if (cellValue == "")
+                    {
+                        break;
+                    }
+                    if (listBox1.Items.Contains(cellValue))
+                    {
+                        continue;
+                    }
+                    listBox1.Items.Add(cellValue);
+                    count++;
+                }
+                saveListBox();
+                MessageBox.Show($"Добавлено {count} записей.");
+            }
         }
     }
 }
